0309b1809c38a36dc940b7f9a3ca6394a34210e0
Maxence Bouret <maxence.bouret@gmail.com>
2019-07-19 15:18:54 +0100

    On of language selection and pages for PopularGithub

--- src/components/pages/PopularGithub.jsx
+++ src/components/pages/PopularGithub.jsx
@@ -5,26 +5,31 @@ import axios from "axios";
 export default function PopularGithub() {
   console.log("PopularGithub: render");
   const [repos, setRepos] = useState([]);
+  const [language, setLanguage] = useState("javascript");
+  const [page, setPage] = useState(1);
 
-  // When we write: useEffect(() => myFunction, [])
-  // myFunction is executed only after the 1st render
+  // When we write: useEffect(() => myFunction, [language,page])
+  // myFunction is executed after the 1st render and when language or page is changed
   useEffect(() => {
     console.log("PopularGithub: useEffect (2nd parameter: [])");
     axios
       .get(
-        "https://api.github.com/search/repositories?sort=stars&q=language:javascript"
+        "https://api.github.com/search/repositories?sort=stars&per_page=50&q=language:" +
+          language +
+          "&page=" +
+          page
       )
       .then(response => {
         console.log("DEBUG response.data.items", response.data.items);
-        setRepos(response.data.items) // Change the state and re-render the component
+        setRepos(response.data.items); // Change the state and re-render the component
       });
 
     // In this case, the return is not necessary
     // The clean up function is executed when the component is destroyed
     return () => {
       console.log("PopularGithub: clean up useEffect (2nd parameter: [])");
-    }
-  }, []);
+    };
+  }, [language,page]);
 
   // When we write: useEffect(() => myFunction)
   // myFunction is executed after each render
@@ -32,8 +37,18 @@ export default function PopularGithub() {
     console.log("PopularGithub: useEffect (no 2nd parameter)");
     return () => {
       console.log("PopularGithub: clean up useEffect (no 2nd parameter)");
-    }
-  })
+    };
+  });
+
+  function handleLanguageChange(e) {
+    setRepos([]);
+    setLanguage(e.target.value);
+  }
+
+  function handlePageChange(e) {
+    setRepos([]);
+    setPage(e.target.value)
+  }
 
   return (
     <div>
@@ -41,13 +56,25 @@ export default function PopularGithub() {
 
       <div className="my-5">
         <Label>Select a language</Label>
-        <Input type="select" name="language">
+        <Input
+          type="select"
+          name="language"
+          value={language}
+          onChange={handleLanguageChange}
+        >
           <option value="css">CSS</option>
           <option value="html">HTML</option>
           <option value="javascript">JavaScript</option>
           <option value="python">Python</option>
           <option value="ruby">Ruby</option>
         </Input>
+        <Label>Select a page</Label>
+        <Input
+          type="number"
+          name="page"
+          value={page}
+          onChange={handlePageChange}
+        />
       </div>
 
       <Table>
